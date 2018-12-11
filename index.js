d411decef7ae4965575cde111e55f3e903d54cc7
仁凯 <maxxwill@163.com>
2018-12-11 11:55:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ export default {
   // makes Array unique
   uniqueArray(array){
     return [...new Set(array)];
-  }
+  },
+  splitNumAndStr(str){
+    let num = str.match(/[0-9]+/ig);
+    let string = str.match(/[a-z]+/ig);
+    return {
+      num,
+      string
+    }
+  },
 }
